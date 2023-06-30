e0057d167d238b505a123c43bffe6729c64e43a6
provokss <105228165+provokss@users.noreply.github.com>
2023-06-30 23:30:37 +0200

    Add files via upload

---
+++ tfa 4info/pages/erreur.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8">
+    <title>lifear</title>
+	
+    <link rel="icon" href="../image\lifear.png">
+</head>
+<body> 
+<?php require '../navbar.php';?>
+	<style type="text/css">
+		h1 {
+			font-size: 4em;
+			margin-top: 100px;
+			text-align: center;
+		}
+	</style>
+	<h1>Vous n'avez pas les permision pour acceder a cette page !</h1>
+</body>
+</html>
